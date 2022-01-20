8e7c6ea7fc9e5c30b045398981ebfde34689bb85
tesigmond <tesigmond@gmail.com>
2022-01-20 09:59:28 -0800

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <header className="App-header"></header>
+        <footer>
+          This website is coded by Tory Sigmond and is{" "}
+          <a
+            href="https://github.com/tesigmond/react-weather-project"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced
+          </a>
+        </footer>
+      </div>
+    </div>
+  );
+}
+
+export default App;
